b825e58781fdbba8ee5199b3826ef662574b2524
Tatiana Eremkina <eremkinatatiana5@gmail.com>
2022-08-29 19:15:52 +0300

    Добавлен класс Point и функция вычисления расстояния между 2 точками на плоскости

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -12,11 +12,21 @@ public class MyFirstProgram {
 
 		Rectangle r = new Rectangle(4,6);
 		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " равна " + r.area());
+
+		Point a = new Point(3, 5);
+		Point b = new Point(1, 2);
+
+		System.out.println("Расстояние между 2 точками " + a.x + "," + a.y + " и " + b.x + "," + b.y + " равна " + distance(a, b));
 		
 	}
 
 	public static void hello(String somebody) {
 		System.out.println("Hello, " + somebody + "!");
+	}
+
+	public static double distance (Point a, Point b){
+		double d = ((a.x - b.x) * (a.x - b.x)) + ((a.y - b.y) * (a.y - b.y));
+		return Math.sqrt(d);
 	}
 
 }
