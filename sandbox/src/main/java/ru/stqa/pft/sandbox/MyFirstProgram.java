de69ed3e48a56c46ef8a039e273923185256c937
Tatiana Eremkina <eremkinatatiana5@gmail.com>
2022-08-29 17:01:31 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,9 +3,29 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
 	public static void main(String args[]){
+		hello("world");
+		hello("user");
+		hello("Tatiana");
 
-		System.out.println("Hello, world!");
+		double l = 5;
+		System.out.println("Площадь квадоата со стороной " + l + " равна " + area(l));
+
+		double a = 4;
+		double b = 6;
+		System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " равна " + area(a, b));
 		
+	}
+
+	public static void hello(String somebody) {
+		System.out.println("Hello, " + somebody + "!");
+	}
+
+	public static double area(double len){
+		return len * len;
+	}
+
+	public static double area(double a, double b){
+		return a * b;
 	}
 
 }
