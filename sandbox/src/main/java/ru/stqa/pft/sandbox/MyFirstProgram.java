dfbd9f2f6aa7ca26f43eb9209144383ed2f02b74
Tatiana Eremkina <eremkinatatiana5@gmail.com>
2022-08-29 18:48:36 +0300

    Добавлены классы Квадрат и Прямоугольник и методы к ним

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -7,25 +7,16 @@ public class MyFirstProgram {
 		hello("user");
 		hello("Tatiana");
 
-		double l = 5;
-		System.out.println("Площадь квадоата со стороной " + l + " равна " + area(l));
+		Square s = new Square(5);
+		System.out.println("Площадь квадоата со стороной "  + s.l + " равна " +  s.area());
 
-		double a = 4;
-		double b = 6;
-		System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " равна " + area(a, b));
+		Rectangle r = new Rectangle(4,6);
+		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " равна " + r.area());
 		
 	}
 
 	public static void hello(String somebody) {
 		System.out.println("Hello, " + somebody + "!");
-	}
-
-	public static double area(double len){
-		return len * len;
-	}
-
-	public static double area(double a, double b){
-		return a * b;
 	}
 
 }
