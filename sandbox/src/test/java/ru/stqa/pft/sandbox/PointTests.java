af46743ff22082428940a9f48184cb72f766a6f5
Tatiana Eremkina <eremkinatatiana5@gmail.com>
2022-11-05 18:11:13 +0300

    Исправлены ошибки для ДЗ 3

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -3,15 +3,13 @@ package ru.stqa.pft.sandbox;
 import org.testng.Assert;
 import org.junit.Test;
 
-import static ru.stqa.pft.sandbox.MyFirstProgram.distance;
-
 public class PointTests {
+
   @Test
   public void testDistance(){
     Point a = new Point(3,7);
     Point b = new Point(1, 7);
-    double c = distance(a, b);
-    Assert.assertEquals(c,2.0);
+    Assert.assertEquals(a.distance(b),2.0);
   }
 
 }
